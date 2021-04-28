6b02a2b404e1ba404bef69e9f48d90f8de0c2f56
Jonathan <jandrey@gmail.com>
2021-04-28 16:26:19 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*
+            //variables
+            string itemName = "Widget";
+            double pricePerOnce = 17.36; 
+            string outputString;
+
+            //format output
+            //outputString = String.Format("The current price is {0:C}" , pricePerOnce);
+            outputString = String.Format("{0,10} {1,10}", itemName, pricePerOnce);
+
+
+            //output to the user
+            Console.WriteLine(String.Format("{0,10} {1,10}", "Item Name", "Price"));
+            Console.WriteLine(outputString);
+            
+            */
+
+            string file = @"c:\documents\word\japrep";
+            string file1 = "c:\\documents\\word\\taprep";
+            Console.WriteLine(file);
+            Console.WriteLine(file1);
+
+        }
+    }
+}
